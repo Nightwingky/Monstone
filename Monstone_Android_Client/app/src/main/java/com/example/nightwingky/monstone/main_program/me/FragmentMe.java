d3be3ac62504d1d048d73bc90568decaf1d29c87
Nightwingky <qky1024@163.com>
2017-03-12 22:47:25 +0800

    完成fragmentMe界面布局，listView未测试

--- Monstone_Android_Client/app/src/main/java/com/example/nightwingky/monstone/main_program/me/FragmentMe.java
+++ Monstone_Android_Client/app/src/main/java/com/example/nightwingky/monstone/main_program/me/FragmentMe.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.nightwingky.monstone.R;
 
@@ -15,10 +16,16 @@ import com.example.nightwingky.monstone.R;
 
 public class FragmentMe extends Fragment {
 
+    private ListView lv_me;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view  = inflater.inflate(R.layout.fragment_layout_me, container, false);
+        lv_me = (ListView) view.findViewById(R.id.lv_me);
+
+        MeListViewAdapter adapter = new MeListViewAdapter(FragmentMe.this.getActivity());
+        lv_me.setAdapter(adapter);
 
         return view;
     }
---
+++ Monstone_Android_Client/app/src/main/java/com/example/nightwingky/monstone/main_program/me/MeListViewAdapter.java
@@ -0,0 +1,74 @@
+package com.example.nightwingky.monstone.main_program.me;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.nightwingky.monstone.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nightwingky on 17-3-12.
+ */
+
+public class MeListViewAdapter extends BaseAdapter {
+
+    private List<String> mTextList;
+    private LayoutInflater mInflater;
+
+    public MeListViewAdapter(Context context) {
+        this.mTextList = new ArrayList<String>();
+        this.mTextList.add("我发布的");
+        this.mTextList.add("我卖出的");
+        this.mTextList.add("我买到的");
+        this.mTextList.add("我赞过的");
+
+        this.mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return mTextList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mTextList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if(convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = mInflater.inflate(R.layout.item_fragment_home_listview, null);
+            viewHolder.tvText = (TextView) convertView.findViewById(R.id.txt_item_listView_home);
+            viewHolder.imgIcon = (ImageView) convertView.findViewById(R.id.img_item_listView_home);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.imgIcon.setImageResource(R.mipmap.ic_launcher);
+        viewHolder.tvText.setText(mTextList.get(position));
+
+        return convertView;
+    }
+
+    class ViewHolder {
+        public ImageView imgIcon;
+        public TextView tvText;
+    }
+}
