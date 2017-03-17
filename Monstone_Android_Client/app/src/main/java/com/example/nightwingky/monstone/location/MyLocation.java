e1a5d73e562db85b4475d8469d3aa74bbfb129be
Nightwingky <qky1024@163.com>
2017-03-17 22:06:45 +0800

    gitignore

--- Monstone_Android_Client/app/src/main/java/com/example/nightwingky/monstone/location/MyLocation.java
+++ Monstone_Android_Client/app/src/main/java/com/example/nightwingky/monstone/location/MyLocation.java
@@ -4,6 +4,13 @@ package com.example.nightwingky.monstone.location;
  * Created by nightwingky on 17-3-16.
  */
 
+/*******************************************************
+ * 获取我的SHA-1
+ * Android Studio控制台中输入命令：
+ * keytool -list -v -keystore ~/.android/debug.keystore
+ * 口令：android
+ *******************************************************/
+
 public class MyLocation {
 
 
