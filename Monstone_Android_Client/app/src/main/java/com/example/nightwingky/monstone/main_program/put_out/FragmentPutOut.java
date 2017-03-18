0bfc7eee5bb00594112de81b4e88bc4cf0e97d4a
Nightwingky <qky1024@163.com>
2017-03-18 22:21:11 +0800

    完成本机选择照片功能

--- Monstone_Android_Client/app/src/main/java/com/example/nightwingky/monstone/main_program/put_out/FragmentPutOut.java
+++ Monstone_Android_Client/app/src/main/java/com/example/nightwingky/monstone/main_program/put_out/FragmentPutOut.java
@@ -1,13 +1,30 @@
 package com.example.nightwingky.monstone.main_program.put_out;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.nightwingky.monstone.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by nightwingky on 17-3-12.
@@ -15,11 +32,54 @@ import com.example.nightwingky.monstone.R;
 
 public class FragmentPutOut extends Fragment {
 
+    private ImageView img_upload;
+
+    private Bitmap myBitmap;
+    private byte[] mContent;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_layout_put_out, container, false);
 
+        this.img_upload = (ImageView) view.findViewById(R.id.img_upload_fragment_put_out);
+
+        setListener();
+
         return view;
+    }
+
+    private void setListener() {
+        img_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                // 开启Pictures画面Type设定为image
+                intent.setType("image/*");
+                // 使用Intent.ACTION_GET_CONTENT这个Action
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                // 取得相片后返回本画面
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            Log.e("uri", uri.toString());
+            ContentResolver cr = this.getActivity().getContentResolver();
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                /* 将Bitmap设定到ImageView */
+                img_upload.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                Log.e("Exception", e.getMessage(),e);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
