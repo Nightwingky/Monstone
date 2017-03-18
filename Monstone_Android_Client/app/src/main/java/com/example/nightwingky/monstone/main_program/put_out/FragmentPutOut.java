68114d4c4dcd8dc760f8811bd69ffde4494b2299
Nightwingky <qky1024@163.com>
2017-03-18 22:34:22 +0800

    完善图片选择功能

--- Monstone_Android_Client/app/src/main/java/com/example/nightwingky/monstone/main_program/put_out/FragmentPutOut.java
+++ Monstone_Android_Client/app/src/main/java/com/example/nightwingky/monstone/main_program/put_out/FragmentPutOut.java
@@ -1,16 +1,12 @@
 package com.example.nightwingky.monstone.main_program.put_out;
 
-import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.ContentResolver;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,9 +16,7 @@ import android.widget.ImageView;
 
 import com.example.nightwingky.monstone.R;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -32,17 +26,20 @@ import static android.app.Activity.RESULT_OK;
 
 public class FragmentPutOut extends Fragment {
 
-    private ImageView img_upload;
+    private ImageView img_upload1;
+    private ImageView img_upload2;
+    private ImageView img_upload3;
 
-    private Bitmap myBitmap;
-    private byte[] mContent;
+    private int count;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_layout_put_out, container, false);
 
-        this.img_upload = (ImageView) view.findViewById(R.id.img_upload_fragment_put_out);
+        this.img_upload1 = (ImageView) view.findViewById(R.id.img_upload_fragment_put_out1);
+        this.img_upload2 = (ImageView) view.findViewById(R.id.img_upload_fragment_put_out2);
+        this.img_upload3 = (ImageView) view.findViewById(R.id.img_upload_fragment_put_out3);
 
         setListener();
 
@@ -50,18 +47,39 @@ public class FragmentPutOut extends Fragment {
     }
 
     private void setListener() {
-        img_upload.setOnClickListener(new View.OnClickListener() {
+        img_upload1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                // 开启Pictures画面Type设定为image
-                intent.setType("image/*");
-                // 使用Intent.ACTION_GET_CONTENT这个Action
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                // 取得相片后返回本画面
-                startActivityForResult(intent, 1);
+                count = 1;
+                startImgActivity();
             }
         });
+
+        img_upload2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = 2;
+                startImgActivity();
+            }
+        });
+
+        img_upload3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = 3;
+                startImgActivity();
+            }
+        });
+    }
+
+    private void startImgActivity() {
+        Intent intent = new Intent();
+        // 开启Pictures画面Type设定为image
+        intent.setType("image/*");
+        // 使用Intent.ACTION_GET_CONTENT这个Action
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        // 取得相片后返回本画面
+        startActivityForResult(intent, 1);
     }
 
     @Override
@@ -74,10 +92,20 @@ public class FragmentPutOut extends Fragment {
             ContentResolver cr = this.getActivity().getContentResolver();
             try {
                 Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
-                /* 将Bitmap设定到ImageView */
-                img_upload.setImageBitmap(bitmap);
+                switch (count) {
+                    case 1:
+                        img_upload1.setImageBitmap(bitmap);
+                        break;
+                    case 2:
+                        img_upload2.setImageBitmap(bitmap);
+                        break;
+                    case 3:
+                        img_upload3.setImageBitmap(bitmap);
+                        break;
+                }
+
             } catch (FileNotFoundException e) {
-                Log.e("Exception", e.getMessage(),e);
+                Log.e("Exception", e.getMessage(), e);
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
